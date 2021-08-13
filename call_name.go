e5bc395a0527e8677284de0fb114528389808ddb
M Nouval A Alkaff <abdelnouval@gmail.com>
2021-08-13 08:38:46 +0700

    rollback to version 1.6.0

--- call_name.go
+++ call_name.go
@@ -3,5 +3,5 @@ package go_call_name
 import "strconv"
 
 func CallName(name string, age int) interface{} {
-	return "Hello " + name + "are you already" + strconv.Itoa(int(age)) + "?"
+	return "Hello " + name + " are you already " + strconv.Itoa(int(age)) + "?"
 }
