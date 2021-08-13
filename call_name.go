1998f390e49d9f6ac925ec607ee6318692febe20
M Nouval A Alkaff <abdelnouval@gmail.com>
2021-08-13 08:48:44 +0700

    update to version 2.0.0

--- call_name.go
+++ call_name.go
@@ -2,6 +2,7 @@ package go_call_name
 
 import "strconv"
 
+// go mod download "module name"
 func CallName(name string, age int) interface{} {
 	return "Hello " + name + " are you already " + strconv.Itoa(int(age)) + "?"
 }
