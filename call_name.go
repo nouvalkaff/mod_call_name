09ce7bbad1356b4d88f5f8f71424a66b5db65a75
M Nouval A Alkaff <abdelnouval@gmail.com>
2021-08-13 09:23:29 +0700

    update to v2.0.0

--- call_name.go
+++ call_name.go
@@ -2,6 +2,6 @@ package go_call_name
 
 import "strconv"
 
-func CallName(name string, age int) interface{} {
-	return "Hello " + name + " are you already " + strconv.Itoa(int(age)) + "?"
+func CallName(name string, age int, response string) interface{} {
+	return "Hello " + name + " are you already " + strconv.Itoa(int(age)) + "?" + response + " I guess."
 }
